d91976027a1fa29162503750f4423c0463200ab6
The Lame Brain <thelamebrain@gmail.com>
2026-03-14 14:37:26 -0700

    Thumbnail save system

--- RatJam/Assets/Scripts/GameManager.cs
+++ RatJam/Assets/Scripts/GameManager.cs
@@ -1,16 +1,55 @@
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Singleton
+    public static GameManager instance;
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+    #endregion
+
+    #region Save and Load
+    public static string SAVENAME = "Default";
+    private static string path;
+
+    public static void SaveGame()
+    {        
+        if (File.Exists(path)) File.Delete(path);
+        StreamReader sr = new StreamReader(path);
+        //        sw.WriteLine(_character.Save_Character());
+        sr.Close();
     }
 
-    // Update is called once per frame
-    void Update()
+    public static bool LoadGame()
     {
-        
+        if (File.Exists(path))
+        {
+            StreamReader sr = new StreamReader(path);
+            // save file = sr.ReadToEnd();
+            sr.Close();
+            return true;
+        }
+
+        return false;
+    }
+
+
+    #endregion
+
+    private void Start()
+    {
+        path = Application.persistentDataPath + "/" + SAVENAME + "/save.dat";
     }
 }
---
+++ RatJam/Assets/Scripts/SaveGameFile_Class.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SaveGameFile_Class 
+{
+    //Structure for saved game files
+    public string Save_File()
+    {
+        string _result = "";
+        return _result;
+    }
+    public void Load_File(string _savefile)
+    {
+
+    }
+}
