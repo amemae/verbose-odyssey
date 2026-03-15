383c357b61b2ed383c08b3251ba8466ae2ad53b1
Amelia Roberts <amemaeroberts@gmail.com>
2026-03-15 12:48:14 -0700

    feat: Add ITile TileSpriteIndex

--- RatJam/Assets/Scripts/Tile/ITile.cs
+++ RatJam/Assets/Scripts/Tile/ITile.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public interface ITile
 {
     TileType TileType { get => throw new System.NotImplementedException(); }
-
+    int TileSpriteIndex { get => throw new System.NotImplementedException(); }
     protected virtual bool IsWalkable()
     {
         throw new System.NotImplementedException();
